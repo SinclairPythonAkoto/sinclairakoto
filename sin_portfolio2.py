a0ebc988bae046d04bb68a2c08ccfef7be8766d0
SinclairPythonAkoto <sinclair.python@gmail.com>
2020-04-10 08:48:55 +0100

    nike sweater html page & meta data added; layout of jakets css altered into two rows

--- sin_portfolio2.py
+++ sin_portfolio2.py
@@ -88,8 +88,31 @@ def black_gilet_checkout():
     myEmail = CEmail
     # sending an email to the customer
     msg = Message('Thank you for your purchase!', recipients=[myEmail]) # (Email subject, [Customer Email])
-    msg.html = f"<p>Thank you {customer_name} for your recent purchase from My Demo Shop! This is an email confirming that you purchased an {product} for {price}.\n\nAs this is a demo version, you will not recieve a separate email from Stripe confirming your with a reference number.\nYour item(s) will be sent to {street}, {city}, {postcode} and will be dispatched to our courier soon.\nDeliveries usually take between 3 - 7 working days, if your delivery has taken longer than that please contact us with your reference number.\n\nAdditionally, if you would like to continue shopping at My Demo Shop please click <b><a href='https://www.sinclair.codes/demoshop'>here</a></b>.<br><br>\n\nTo leave a comment/review please click <b><a href='https://www.sinclair.codes/demoshop#demoshop_footer'>here</a></b>.</p>"
+    msg.html = f"<p>Thank you {customer_name} for your recent purchase from My Demo Shop! This is an email confirming that you purchased an {product} for {price}.<br><br>As this is a demo version, you will not recieve a separate email from Stripe confirming your with a reference number.<br>Your item(s) will be sent to {street}, {city}, {postcode} and will be dispatched to our courier soon.<br>Deliveries usually take between 3 - 7 working days, if your delivery has taken longer than that please contact us with your reference number.<br><br>Additionally, if you would like to continue shopping at My Demo Shop please click <b><a href='https://www.sinclair.codes/demoshop'>here</a></b>.<br><br>To leave a comment/review please click <b><a href='https://www.sinclair.codes/demoshop#demoshop_footer'>here</a></b>.</p>"
     # add an attachment logo/pdf etc to email
+    with app.open_resource('BondRobotics_logo_crop.JPG') as logo:
+        msg.attach('BondRobotics_logo_crop.JPG', 'image/jpeg', logo.read())
+    mail.send(msg)
+
+    return redirect(url_for('demoshop'))
+
+@app.route('/nike-swoosh-sweater-purchase')
+def nike_swoosh_sweater_purchase():
+    return render_template('nike_swoosh_sweater.html', pub_key=pub_key)
+
+@app.route('/nike-swoosh-sweater-checkout', methods=['POST'])
+def nike_swoosh_sweater_checkout():
+    from payments import NikeSweater_55gbp
+    customer_name = request.form.get("Name")
+    street = request.form.get("Street")
+    city = request.form.get("City")
+    postcode = request.form.get("Postcode")
+    product = "Grey Nike Swoosh Sweater"
+    price = "£55.00"
+    CEmail = request.form['stripeEmail']
+    myEmail = CEmail
+    msg = Message('Thank you for your purchase!', recipients=[myEmail])
+    msg.html = f"<p>Thank you {customer_name} for your recent purchase from My Demo Shop! This is an email confirming that you purchased a {product} for {price}.<br><br>As this is a demo version, you will not recieve a separate email from Stripe confirming your with a reference number.<br>Your item(s) will be sent to {street}, {city}, {postcode} and will be dispatched to our courier soon.<br>Deliveries usually take between 3 - 7 working days, if your delivery has taken longer than that please contact us with your reference number.<br><br>Additionally, if you would like to continue shopping at My Demo Shop please click <b><a href='https://www.sinclair.codes/demoshop'>here</a></b>.<br><br>To leave a comment/review please click <b><a href='https://www.sinclair.codes/demoshop#demoshop_footer'>here</a></b>.</p>"
     with app.open_resource('BondRobotics_logo_crop.JPG') as logo:
         msg.attach('BondRobotics_logo_crop.JPG', 'image/jpeg', logo.read())
     mail.send(msg)
